b85b3036a4754c9d689d23babeaad2ca340bf42c
riteshbehera857 <riteshbehera857@gmail.com>
2020-12-18 14:53:48 +0530

    'main part'

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(`${__dirname}/date.js`);
+const mongoose = require('mongoose');
 
 
 const app = express();
@@ -8,86 +8,113 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = [];
+// mongoose start
+mongoose.connect('mongodb://localhost:27017/toDoListDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Wake up"
+});
+
+const item2 = new Item({
+    name: "Make your bed"
+});
+
+const item3 = new Item({
+    name: "Start your day"
+})
+
+const defaultItems = [item1, item2, item3];
+
+
+
+//mongoose finshed
+
 const workItems = [];
 
 app.get("/", (req, res) => {
 
-    // to get the current date or week name
+    Item.find({}, (err, foundItems) => {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('successfull !');
+                }
+            })
+            res.redirect('/');
+        } else {
+            res.render("list", {
+                listTitle: "Today",
+                newItems: foundItems
+            });
+        }
 
-    let day = date();
 
+    })
 
-    res.render("list", {
-        listTitle: day,
-        newItem: items
-    });
-});
+})
 
 app.post("/", (req, res) => {
 
-    let item = req.body.toDoList;  
-    let workItem = req.body.toDoList;
+    let newItem = req.body.toDoList;
 
-    if (req.body.list === "Work") {
-        workItems.push(workItem);
-        res.redirect("/work");
-    }
-    else {
-        items.push(item);
-        res.redirect("/");
-    }   
-})
-
-app.get("/work", (req, res) => {
-
-
-    res.render("list", {
-        listTitle: "Work List",
-        newItem: workItems
+    const item = new Item({
+        name: newItem
     });
+
+    item.save();
+
+    res.redirect('/');
+
+
+    // let workItem = req.body.toDoList;
+
+    // if (req.body.list === "Work") {
+    //     workItems.push(workItem);
+    //     res.redirect("/work");
+    // }
+    // else {
+    //     items.push(item);
+    //     res.redirect("/");
+    // }
 })
+
+app.post('/delete', (req, res) => {
+    const itemId = req.body.checkbox;
+    // console.log(itemId);
+    Item.findByIdAndRemove(itemId, (err) => {
+        if (!err) {
+            console.log("succesfully deleted the Item");
+        } else {
+            console.log(err, "We are unable to delete the Item");
+        }
+    })
+
+    res.redirect('/');
+});
+
+app.get("/:parameter", (req, res) => {
+    console.log(req.params.parameter);
+})
+
+
+// app.get("/work", (req, res) => {
+
+
+//     res.render("list", {
+//         listTitle: "Work List",
+//         newItem: workItems
+//     });
+// })
 
 
 app.listen(3000, () => {
     console.log("server is running at port 3000");
 });
-
-
-
-/*
- the concept of scope:
-
- function a {
- let x = something
-
- console.log(x);
-
- //output willbe something.
-
- //these ar called Local variables
-
-
- in this case i can only call the variable x inside the function a,which might give us some problem.
- }
-
-*/
-
-/*
-
-
-var a = 2
-
-function x {
-    console.log(a)
-
-}
-
-console.log(a);
-
-//these are called Global variables.
-
-
-//in this case we can call the variable both inside or outside the variable.
-
-*/
