793356ec99239886ffb2c6b21d60b116c9927535
riteshbehera857 <riteshbehera857@gmail.com>
2020-12-19 20:21:12 +0530

    last

--- app.js
+++ app.js
@@ -137,6 +137,6 @@ app.post("/delete", function (req, res) {
 
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("server is running at port 3000");
 });
